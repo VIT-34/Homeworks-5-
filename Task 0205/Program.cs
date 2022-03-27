6128e9e914ed4505b9a06e1b3ac8bc0dc82edb97
Vitaliy <viurum986@mail.ru>
2022-03-27 21:32:32 +0300

    Init Task 0205

---
+++ Task 0205/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int length = 10;
+int[] array = new int[length];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < length; i++)
+{
+    array[i] = rnd.Next(1, 70);
+    Console.WriteLine(array[i]);
+    if (array[i] % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine(sum);
