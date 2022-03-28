c1de54e82e968d55409185f7f26929bb077d9e5c
Vitaliy <viurum986@mail.ru>
2022-03-28 21:58:29 +0300

    init Task 0305

--- Task 0305/Program.cs
+++ Task 0305/Program.cs
@@ -1,20 +1,29 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-double min = double.MaxValue;
-            double max = double.MinValue;
- 
-            double k;
- 
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write("Введите {0}-е число: ", i + 1);
-                k = double.Parse(Console.ReadLine());
- 
-                if (k > max)
-                    max = k;
- 
-                if (k < min)
-                    min = k;
-            }
- 
-            Console.WriteLine("Разница между макс и мин: {0}", max - min);
+
+double min = double.MaxValue; 
+double max = double.MinValue; 
+double k;
+
+double[] array = new double[10];
+Random rand = new Random();
+Console.WriteLine("Заполненый массив");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = min;
+    Console.WriteLine();
+
+        k = rand.NextDouble() * 100/7;
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+    
+    if (k > max)
+        max = k;
+
+    if (k < min)
+        min = k;
+}
+
+Console.WriteLine("Разница между макс и мин: {0:0.000}", max - min);
+    
+
